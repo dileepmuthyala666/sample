454f63e32e867eae34aedd0309ed06604da8b866
dileepmuthyala666 <142497211+dileepmuthyala666@users.noreply.github.com>
2023-08-22 09:58:56 +0530

    Add files via upload

---
+++ Twoarray.java
@@ -0,0 +1,16 @@
+public class Twoarray {
+
+	public static void main(String[] args) {
+					
+		    int []a= {1,2,3,4};
+		    int []b= {2,3,4,5};
+		    int []c= new int[4];
+			System.out.println("Evalute Addition Of Two Arrays ");
+		    for(int i=0;i<4;i++)
+				{
+						c[i]=a[i]+b[i];	
+					System.out.print(c[i]+" ");	
+				}
+				
+			}
+}
